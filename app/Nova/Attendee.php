6bcdc4972002f1518cfc1f7f44e552cb32d57123
Nik Lub <nlubyanoy@fluxergy.com>
2023-01-30 12:17:01 -0800

    Removed required for church on admin page

--- app/Nova/Attendee.php
+++ app/Nova/Attendee.php
@@ -52,7 +52,7 @@ class Attendee extends Resource
                 ->displayUsing(fn () => $this->stripe_id ? env('STRIPE_DASHBOARD_URL', 'https://dashboard.stripe.com/') . 'customers/' . $this->stripe_id : '')
                 ->sortable(),
             DateTime::make('Email Verified At')->sortable(),
-            Text::make('Church')->sortable()->rules('required'),
+            Text::make('Church')->sortable(),
             Select::make('Sex')->sortable()->options(['m' => 'Male', 'f' => 'Female'])->rules('required'),
             Password::make('Password')->onlyOnForms(),
         ];
